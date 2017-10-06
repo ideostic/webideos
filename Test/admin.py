6d9656da86e400373fb9ffd847465d2d1562fec3
Roger José <roger357war@gmail.com>
2017-10-06 13:30:23 -0500

    Models - Cambios menores en el archivo de mapeo de modelos para la administracion.

--- Test/admin.py
+++ Test/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from Test.models import *
 
 
-admin.site.register(Country)
-admin.site.register(User)
-admin.site.register(StudyType)
-admin.site.register(LaboralExp)
-admin.site.register(StudyHistory)
-admin.site.register(Degree)
 admin.site.register(PhoneCountryCode)
-admin.site.register(PhoneType)
+admin.site.register(StudyHistory)
 admin.site.register(PhoneNumber)
+admin.site.register(LaboralExp)
 admin.site.register(Habilities)
+admin.site.register(PhoneType)
+admin.site.register(StudyType)
 admin.site.register(Aptitudes)
+admin.site.register(Country)
+admin.site.register(Degree)
+admin.site.register(User)
+
