31a3343a45f29e398246c6b2e7d953672dea26d3
roger357 <roger357war@gmail.com>
2017-10-05 23:26:13 -0500

    Models - Cambios para la gestion de la data de los modelos desde el panel administrativo.

--- Test/admin.py
+++ Test/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from Test.models import *
+
+
+admin.site.register(Country)
+admin.site.register(User)
+admin.site.register(StudyType)
+admin.site.register(LaboralExp)
+admin.site.register(StudyHistory)
+admin.site.register(Degree)
+admin.site.register(PhoneCountryCode)
+admin.site.register(PhoneType)
+admin.site.register(PhoneNumber)
+admin.site.register(Habilities)
+admin.site.register(Aptitudes)
