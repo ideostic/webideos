2507ffaf3773f86646dd948cde7f2bfbeac31bc5
Roger José <roger357war@gmail.com>
2017-10-11 16:52:34 -0500

    Inclusion de index.HTML y archivos estaticos en las vistas (.js, bootstrap, .css, JQuery).
    El mapeo de los archivos es posible gracias a la propiedad STATIC_URL.
    
    Agrego (Para el futuro) propiedad para el mapeo de multiples directoris contenedores de recursos staticos STATICFILES_DIRS.
    
    Se modifica el archivo views para que renderise y retorne el index.html

--- toc/settings.py
+++ toc/settings.py
@@ -120,4 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#ESTE SE UTILIZA PARA CUANDO SE DESEAN UTILIZAR MAS DE UN DIRECTORIA PARA LOS ARCHIVOS ESTATICOS.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
